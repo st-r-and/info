4049b5746e898504b7001420b1ec87a68ef46762
Steve Reichel <foren@fam-reichel.de>
2011-07-18 22:38:34 +0200

    admin

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Translation//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title> Eingebe</title>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+</head>
+<body>
+<h1>Eingabe</h1>
+<a href='prognose.php'>Prognose</a><br />
+<a href='back.php'>Pflegen</a><br />
+</body>
+</html>
