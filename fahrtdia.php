115d55a67e34cd443922b745c587e6c4d45eb811
Steve Reichel <foren@fam-reichel.de>
2011-07-25 19:42:31 +0200

    fahtdia null prop

--- fahrtdia.php
+++ fahrtdia.php
@@ -9,12 +9,12 @@
   $prozent = 100 / htmlspecialchars($gesammt) * htmlspecialchars($gekommen);
   $winkel = 360 / 100 * $prozent;
   header("Content-type: image/gif");
-$gro = 400;
+  $gro = 400;
   $bild = imagecreatetruecolor($gro, $gro);
   $farbe_hintergrund = imagecolorexact($bild, 255, 204, 102);
   imagefill($bild, 0, 0, $farbe_hintergrund);
   $red   = imagecolorallocate($bild, 255,   0,   0);
-  imagefilledarc($bild, $gro/2, $gro/2, $gro-10, $gro-10,  180, 180 + $winkel, $red, IMG_ARC_PIE);
+if ($winkel > 0) imagefilledarc($bild, $gro/2, $gro/2, $gro-10, $gro-10,  180, 180 + $winkel, $red, IMG_ARC_PIE);
   imagegif($bild);
   imagedestroy($bild);
 ?>
