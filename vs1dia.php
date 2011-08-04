6a6774eaf23408ea6164e9a3c09d8cc09f8184df
Steve Reichel <foren@fam-reichel.de>
2011-08-04 22:59:30 +0200

    alle vsdia ext

--- vs1dia.php
+++ vs1dia.php
@@ -1,34 +1,8 @@
 <?php
-  require_once "admin/daten.php";
-  if ($lesen = $datab->prepare("SELECT vs1, vs2, vs3, vs4, vs5 FROM stoerung WHERE id=1")) {
-      $lesen->execute();
-      $lesen->bind_result($vs1, $vs2, $vs3, $vs4, $vs5);
-      $lesen->fetch();
-      $lesen->close();
-  }
-  header("Content-type: image/gif");
-  $max = $vs1;
-  if($vs2 > $max) $max = $vs2;
-  if($vs3 > $max) $max = $vs3;
-  if($vs4 > $max) $max = $vs4;
-  if($vs5 > $max) $max = $vs5;
-  $hoehe = 365;
-  $breite = 100;
-  $rand = 25;
-  $bild = imagecreatetruecolor($breite, $hoehe);
-  $farbe_hintergrund = imagecolorexact($bild, 255, 204, 102);
-  $lime = imagecolorallocate($bild, 0x00, 0xFF, 0x00);
-  imagecolortransparent($bild, $lime);
-  imagefill($bild, 0, 0, $farbe_hintergrund);
-  $black = imagecolorallocate($bild, 0,   0,   0);
-  $red   = imagecolorallocate($bild, 255,   0,   0);
-if ($max > 0) {
-  $prozent = 100 / $max * $vs1;
-  $aussl = ( $hoehe - 2 * $rand ) / 100 * $prozent;
-  imagefilledrectangle($bild, $rand, $hoehe - $rand - $aussl, $breite - $rand, $hoehe - $rand, $red);
-  imagerectangle($bild, $rand, $rand, $breite - $rand, $hoehe -$rand, $black);
-}
-  imagegif($bild);
-  imagedestroy($bild);
+require_once "vsalldia.php";
+header("Content-type: image/gif");
+baue($vs1);
+imagegif($bild);
+imagedestroy($bild);
 $datab->close();
 ?>
